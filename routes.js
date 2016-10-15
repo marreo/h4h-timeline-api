14cc8d7f2785a03cc1d07888ee2c927da5586478
Martin Lindgren <martin@marreomedia.se>
2016-10-15 20:13:34 +0200

    Fixed ids in xml Parse between xml and son

--- routes.js
+++ routes.js
@@ -4,26 +4,41 @@ var fs = require('fs');
 var xpath = require('xpath')
     , dom = require('xmldom').DOMParser
 var unique = require('array-unique');
+var parser = require('xml2json');
 const user = require('./model/user/user-router');
 router.route('/api').get((req, res) => {
     var parse = require('xml-parser');
     var xmlHafte = fs.readFileSync('data/rothafte.xml', 'utf8');
     var docHafte = new dom().parseFromString(xmlHafte);
-    var nodesHafte = xpath.select("//rotemansarkivethaften[Kodtext = 'Katarina']", docHafte);
+    var loc = req.query.loc;
+    console.log('loc: ' + loc);
+    var nodesHafte = xpath.select("//rotemansarkivethaften[Kodtext = '" + loc + "' and Kod='GATA']", docHafte);
     var xmlBase = fs.readFileSync('data/rotbas.xml', 'utf8');
     var docBase = new dom().parseFromString(xmlBase);
+    var xmlArr = null;
+    var xmlStr = "";
     nodesHafte.forEach(function (node) {
-        console.log("Node: " + node);
         const myReg = />(\d*)</g;
         var id = myReg.exec(node)[1];
         console.log("Fetchzed id: " + id);
         console.log("trying to execute xpath: " + "//rotemansarkivetbas[Häftesnummer = '" + id + "']");
         var match = xpath.select("//rotemansarkivetbas[Häftesnummer = '" + id + "']", docBase);
-        console.log("Found one!" + match.length);
+        console.log("L:" + match.length);
+        if (match.length > 0) {
+            console.log("Found one!" + match);
+            xmlArr = match;
+        }
     });
+    xmlStr = '<?xml version="1.0" encoding="utf-8" standalone="yes"?><data>' + xmlArr.join('') + '</data>';
     console.log("num of posts: " + nodesHafte.length);
-    var obj = JSON.parse(fs.readFileSync('data/data.json', 'utf8'));
+    console.log("xmlStr: " + xmlStr);
+    var obj = null;
+    if (nodesHafte.length > 0) {
+        var json = parser.toJson(xmlStr);
+        obj = JSON.parse(json);
+    }
     res.jsonp(obj);
 });
 router.use('/user', user);
 module.exports = router;
+//Urvädersgränd,8
