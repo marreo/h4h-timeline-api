5e653a88b1468baa83b7f02b2c2cd06e3b6bc728
Martin Lindgren <martin@marreomedia.se>
2016-10-15 18:34:17 +0200

    Added rothafte.xml Code to read, parse and query xml files

--- model/pet/pet-controller.js
+++
@@ -1,7 +0,0 @@
-const Controller = require('../../lib/controller');
-const petModel  = require('./pet-facade');
-
-
-class PetController extends Controller {}
-
-module.exports = new PetController(petModel);
--- routes.js
+++ routes.js
@@ -1,25 +1,29 @@
 const Router = require('express').Router;
 const router = new Router();
 var fs = require('fs');
-
-const user  = require('./model/user/user-router');
-const pet  = require('./model/pet/pet-router');
-
-
+var xpath = require('xpath')
+    , dom = require('xmldom').DOMParser
+var unique = require('array-unique');
+const user = require('./model/user/user-router');
 router.route('/api').get((req, res) => {
-    
+    var parse = require('xml-parser');
+    var xmlHafte = fs.readFileSync('data/rothafte.xml', 'utf8');
+    var docHafte = new dom().parseFromString(xmlHafte);
+    var nodesHafte = xpath.select("//rotemansarkivethaften[Kodtext = 'Katarina']", docHafte);
+    var xmlBase = fs.readFileSync('data/rotbas.xml', 'utf8');
+    var docBase = new dom().parseFromString(xmlBase);
+    nodesHafte.forEach(function (node) {
+        console.log("Node: " + node);
+        const myReg = />(\d*)</g;
+        var id = myReg.exec(node)[1];
+        console.log("Fetchzed id: " + id);
+        console.log("trying to execute xpath: " + "//rotemansarkivetbas[Häftesnummer = '" + id + "']");
+        var match = xpath.select("//rotemansarkivetbas[Häftesnummer = '" + id + "']", docBase);
+        console.log("Found one!" + match.length);
+    });
+    console.log("num of posts: " + nodesHafte.length);
     var obj = JSON.parse(fs.readFileSync('data/data.json', 'utf8'));
-    
-    var name = req.param("n");
-    var address = req.param("a");
-    
-    console.log("Params: n >" + name + " a " + address);
-    
-  res.jsonp(obj);
+    res.jsonp(obj);
 });
-
 router.use('/user', user);
-router.use('/pet', pet);
-
-
 module.exports = router;
